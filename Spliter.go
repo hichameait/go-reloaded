82774e529b0304a2d12e669fff04596b78118374
HICHAME AIT BENALLA <its.aitbenalla.hichame@gmail.com>
2025-11-30 09:23:57 +0100

    Refactor Spliter function and improve readability

--- Spliter.go
+++ Spliter.go
@@ -4,8 +4,11 @@ import (
 	"strings"
 )
 
-func check(str string, ind int) bool {
-	for i := ind; i < len(str); i++ {
+
+
+
+func check(str string, in int) bool {
+	for i := in; i < len(str); i++ {
 		if str[i] == '(' {
 			return true
 		}
@@ -16,128 +19,59 @@ func check(str string, ind int) bool {
 	return false
 }
 
-func Spliter(str string) []string {
+func Spliter(str string) []string  {
 	textcopy := ""
-	var myarr []string
-	nextext := []rune(str)
+	var Str []string
+	r := []rune(str)
 	i := 0
-	flage := false 
+	flage := false
+	for i = 0; i < len(r)-1; i++ {
 
-	for i = 0; i < len(nextext); i++ {
-		ch := nextext[i]
-		if ch == '('  && i-1 >=0 && nextext[i-1] == ' ' {
-			cmd := ""
-			k := i
-
-			for k < len(nextext) {
-				cmd += string(nextext[k])
-				if nextext[k] == ')' {
-					break
-				}
-				k++
-			}
-			if strings.HasPrefix(cmd, "(cap") || strings.HasPrefix(cmd, "(up") || strings.HasPrefix(cmd, "(low") {
-
-				flage = false
-				if strings.TrimSpace(textcopy) != "" {
-					myarr = append(myarr, strings.TrimSpace(textcopy))
-				}
-				textcopy = ""
-				myarr = append(myarr, strings.TrimSpace(cmd))
-				i = k
-				continue
-			}
-
+		switch r[i] {
+		case '(':
 			flage = true
-			if strings.TrimSpace(textcopy) != "" {
-				myarr = append(myarr, strings.TrimSpace(textcopy))
-			}
-			textcopy = "("
-			continue
+		case ')':
+			flage = false
 		}
-
-		if ch == ')' {
-			if flage {
-				textcopy += ")"
-				myarr = append(myarr, strings.TrimSpace(textcopy))
-				textcopy = ""
-				flage = false
-				continue
-			}
-		}
-		
 		if flage {
-			if ch == ' ' {
-				if strings.TrimSpace(textcopy) != "" {
-					myarr = append(myarr, strings.TrimSpace(textcopy))
+			if check(str, i) && r[i] == ' ' {
+				if textcopy != "" {
+					// Str = append(Str, textcopy)
+					Str = append(Str, strings.TrimSpace(textcopy))
+					textcopy = ""
 				}
+			}
+		}
+
+		if flage {
+			textcopy += string(r[i])
+			continue
+		}
+		if r[i] != '\n' && r[i] != ' ' {
+			textcopy += string(r[i])
+		}
+		if r[i] == '\n' {
+			textcopy += "\n"
+		}
+
+		if r[i] == ' ' && !flage {
+			if textcopy != "" {
+				// Str = append(Str, textcopy)
+				Str = append(Str, strings.TrimSpace(textcopy))
 				textcopy = ""
-				continue
 			}
-			textcopy += string(ch)
-			continue
 		}
+	}
+	if i < len(r) {
 
-		if ch == ' ' {
-			if strings.TrimSpace(textcopy) != "" {
-				myarr = append(myarr, strings.TrimSpace(textcopy))
-			}
+		textcopy += string(r[i])
+		if textcopy != "" {
+			// Str = append(Str, textcopy)
+			Str = append(Str, strings.TrimSpace(textcopy))
 			textcopy = ""
-			continue
+
 		}
-
-		textcopy += string(ch)
 	}
 
-	if strings.TrimSpace(textcopy) != "" {
-		myarr = append(myarr, strings.TrimSpace(textcopy))
-	}
-	return myarr
-
+	return Str
 }
-
-/// old spliter
-// func Spliter(str string) []string {
-// 	var out []string
-// 	part := ""
-// 	flg := true
-// 	// str = CleanSymbols(str)
-// 	// str = CleanQuotes(str)
-// 	r := []rune(str)
-
-// 	for i := 0; i < len(r); i++ {
-// 		ch := r[i]
-// 		if ch == '(' && i+1 <len(r) && r[i+1] != ' '{
-// 			if strings.TrimSpace(part) != "" {
-// 				out = append(out, strings.TrimSpace(part))
-// 			}
-// 			part = "("
-// 			flg = false
-// 			continue
-// 		}
-
-// 		if ch == ')' && i+1 < len(r) && r[i+1] == ' ' {
-// 			part += ")"
-// 			out = append(out, strings.TrimSpace(part))
-// 			part = ""
-// 			flg = true
-// 			continue
-// 		}
-
-// 		if ch == ' ' && flg {
-// 			if strings.TrimSpace(part) != "" {
-// 				out = append(out, strings.TrimSpace(part))
-// 			}
-// 			part = ""
-// 			continue
-// 		}
-
-// 		part += string(ch)
-// 	}
-
-// 	if strings.TrimSpace(part) != "" {
-// 		out = append(out, strings.TrimSpace(part))
-// 	}
-
-// 	return out
-// }
