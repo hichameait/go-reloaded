0016bbd4f1444f54267058a6b6af7c926a5a3a79
HICHAME AIT BENALLA <its.aitbenalla.hichame@gmail.com>
2025-11-30 09:24:35 +0100

    Refactor Vowel function for clarity and efficiency

--- Vowel.go
+++ Vowel.go
@@ -1,7 +1,6 @@
 package functions
 
 import (
-	// "fmt"
 	"strings"
 )
 
@@ -27,10 +26,12 @@ func spliter(str string) []string {
 func Vowel(NewStr []string) string {
 	temp := ""
 	for i, va := range NewStr {
-		if va == "(" && i-2 >= 0 && NewStr[i-2] == ")" {
-			temp += va
+		if (va == "(" || va == ")") && i+1 < len(NewStr) && (NewStr[i+1] == "(" || NewStr[i+1] == ")" || NewStr[i+1] == "[" || NewStr[i+1] == "]") {
+			temp += string(va)
+		} else if (va == "[" || va == "]") && i+1 < len(NewStr) && (NewStr[i+1] == "(" || NewStr[i+1] == ")") {
+			temp += string(va)
 		} else {
-			temp += va + " "
+			temp += string(va) + " "
 		}
 	}
 	test := spliter(temp)
@@ -43,7 +44,7 @@ func Vowel(NewStr []string) string {
 		}
 		if len(NewStr[a]) == 2 {
 			r := []rune(NewStr[a])
-			if r[0] == 39 || r[0] == 40 {
+			if r[0] == 39 || r[0] == 40 || r[0] == 34 {
 				if r[1] == 'A' && a+1 < len(NewStr) {
 					if NewStr[a+1] == " " || NewStr[a+1] == "" {
 						continue
@@ -68,30 +69,35 @@ func Vowel(NewStr []string) string {
 					}
 				}
 			}
-			if NewStr[a] == "A" && a+1 < len(NewStr) {
-				if NewStr[a+1] == " " || NewStr[a+1] == "" {
-					continue
-				}
-				runes := []rune(NewStr[a+1])
-				next := runes[0]
-				if next == 'a' || next == 'e' || next == 'i' || next == 'o' || next == 'u' || next == 'h' ||
-					next == 'A' || next == 'E' || next == 'I' || next == 'O' || next == 'U' || next == 'H' {
-					laststr += "An "
-					continue
-				}
-			} else if NewStr[a] == "a" && a+1 < len(NewStr) {
-				if NewStr[a+1] == " " || NewStr[a+1] == "" {
-					continue
-				}
-				runes := []rune(NewStr[a+1])
-				next := runes[0]
-				if next == 'a' || next == 'e' || next == 'i' || next == 'o' || next == 'u' || next == 'h' ||
-					next == 'A' || next == 'E' || next == 'I' || next == 'O' || next == 'U' || next == 'H' {
-					laststr += "an "
-					continue
-				}
+		}
+		if NewStr[a] == "A" && a+1 < len(NewStr) {
+			if NewStr[a+1] == " " || NewStr[a+1] == "" {
+				continue
+			}
+			runes := []rune(NewStr[a+1])
+			next := runes[0]
+			if next == 'a' || next == 'e' || next == 'i' || next == 'o' || next == 'u' || next == 'h' ||
+				next == 'A' || next == 'E' || next == 'I' || next == 'O' || next == 'U' || next == 'H' {
+				laststr += "An "
+				continue
+			}
+		} else if NewStr[a] == "a" && a+1 < len(NewStr) {
+			if NewStr[a+1] == " " || NewStr[a+1] == "" {
+				continue
+			}
+			runes := []rune(NewStr[a+1])
+			next := runes[0]
+			if next == 'a' || next == 'e' || next == 'i' || next == 'o' || next == 'u' || next == 'h' ||
+				next == 'A' || next == 'E' || next == 'I' || next == 'O' || next == 'U' || next == 'H' {
+				laststr += "an "
+				continue
 			}
 		}
+		// fmt.Println(NewStr[a])
+		// if a+1 < len(NewStr) && NewStr[a+1] == ")" {
+		// 	laststr += NewStr[a]
+		// 	continue
+		// }
 		laststr += NewStr[a] + " "
 	}
 
